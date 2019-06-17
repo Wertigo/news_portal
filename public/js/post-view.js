6f299bc925e12a0c673234259c8c9e5c3cf6aacc
Daniil Karnaukhov <daniil.karnaukhov@noveogroup.com>
2019-06-17 15:06:06 +0300

    likes/dislikes

---
+++ public/js/post-view.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    const $ratingUpButton = $('#rating-up');
+    const upUrl = $ratingUpButton.data('url');
+    const $ratingDownButton = $('#rating-down');
+    const downUrl = $ratingDownButton.data('url');
+    const $ratingCounter = $('#rating-counter');
+
+    const handleRatingChangeResponse = function (response) {
+        const rating = response.rating;
+
+        if (rating !== undefined) {
+            $ratingCounter.text(rating);
+        }
+    };
+
+    $ratingUpButton.click(function () {
+        $.ajax({
+            type: 'POST',
+            url: upUrl,
+            dataType: 'json',
+            success: handleRatingChangeResponse
+        });
+    });
+
+    $ratingDownButton.click(function () {
+        $.ajax({
+            type: 'POST',
+            url: upUrl,
+            dataType: 'json',
+            success: handleRatingChangeResponse
+        });
+    });
+});
---
+++ src/Controller/Api/PostController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Post;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class PostController extends AbstractController
+{
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+
+    /**
+     * PostController constructor.
+     * @param ObjectManager $manager
+     */
+    public function __construct(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @param Post $post
+     * @return JsonResponse
+     */
+    public function ratingUp(Post $post): JsonResponse
+    {
+        $post->setRating($post->getRating() + 1);
+        $this->manager->persist($post);
+        $this->manager->flush();
+
+        return $this->json(['rating' => $post->getRating()]);
+    }
+
+    /**
+     * @param Post $post
+     * @return JsonResponse
+     */
+    public function ratingDown(Post $post): JsonResponse
+    {
+        $post->setRating($post->getRating() - 1);
+        $this->manager->persist($post);
+        $this->manager->flush();
+
+        return $this->json(['rating' => $post->getRating()]);
+    }
+}
